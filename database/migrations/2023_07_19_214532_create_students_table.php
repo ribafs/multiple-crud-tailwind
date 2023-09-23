8fc6ccb4908621a2317f306599991b20dae33eb6
Ribamar FS <ribafs@gmail.com>
2023-09-23 12:30:18 -0300

    Update 2023_07_19_214532_create_students_table.php

--- database/migrations/2023_07_19_214532_create_students_table.php
+++ database/migrations/2023_07_19_214532_create_students_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('students', function (Blueprint $table) {
             $table->id();
             $table->string('firstname');
-            $table->string('lasttname');
+            $table->string('lastname');
             $table->string('email')->unique();
             $table->string('phone');
             $table->timestamps();
